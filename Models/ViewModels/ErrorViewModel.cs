ef28c57c854ac3a1d86bc472f5bb0440174d1508
Kauã <kaua.borba03@gmail.com>
2024-10-05 23:42:41 -0300

    first commit

---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactSystem.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
